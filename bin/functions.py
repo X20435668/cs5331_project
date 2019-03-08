cb9ba10c8f03b331f600271d137805026f7985ab
Xu Yudong <justinxydat@gmail.com>
2019-03-09 00:37:26 +0800

    Add data Samples

--- bin/functions.py
+++ bin/functions.py
@@ -1,18 +1,21 @@
 import sys
 import logging
 import logging.handlers as handlers
-from pathlib import Path
-home = str(Path.home())
+import os
+import os.path
 
 logger = None
 def setup_log():
     logger = logging.getLogger('telegram-bot')
     logger.setLevel(logging.DEBUG)
+    log_dir = "../log"
+    if ( not path.exists(log_dir)) or (not path.isdir(log_dir)):
+        os.mkdir(log_dir)
     # create file handler that logs debug and higher level messages
-    fh = handlers.RotatingFileHandler(home+'/.update/log/update_activity.log',maxBytes=20*1024*1024,backupCount=5)
+    fh = handlers.RotatingFileHandler(path.join(log_dir,'activity.log'),maxBytes=20*1024*1024,backupCount=5)
     fh.setLevel(logging.DEBUG)
     # create console handler with a higher log level
-    ch = handlers.RotatingFileHandler(home+'/.update/log/update_error.log',maxBytes=20*1024*1024,backupCount=5)
+    ch = handlers.RotatingFileHandler(path.join(log_dir,'error.log'),maxBytes=20*1024*1024,backupCount=5)
     ch.setLevel(logging.ERROR)
     # create formatter and add it to the handlers
     ih=logging.StreamHandler()
